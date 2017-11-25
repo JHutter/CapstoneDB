344328249363a47a2bb3d83c2cfbe6691d1d918a
Jon Hutter <jon.elliot.hutter@gmail.com>
2017-11-25 12:15:50 -0800

    default login info for demo purposes

--- php/login_form.php
+++ php/login_form.php
@@ -1,10 +1,29 @@
 <div id="login">
 			<form action="" method="post">
 			<label style="white-space:pre;"><strong>Username :</strong></label>
-			<input id="name" name="username" placeholder="username" type="text">
+			<input id="name" name="username" placeholder="
+				<?php 
+					if (isset($default_username) AND !empty($default_username)){ 
+						print $default_username;}
+					else {
+						print "username";
+					}
+				
+				?>
+				" type="text">
 			<br><br>
 			<label style="white-space:pre;"><strong>Password : </strong></label>
-			<input id="password" name="password" placeholder="**********" type="password">
+			<input id="password" name="password" placeholder="
+			<?php 
+					if (isset($default_pass) AND !empty($default_pass)){ 
+						print $default_pass;}
+					else {
+						print "**********";
+					}
+				
+				?>
+			
+			" type="password">
 			<br><br>
 			<input class="button" name="submit" type="submit" value="      Login      ">
 			<span><script>var isError = '<?php echo $error; ?>'; if (isError != "") {alert('Invalid password or username. Please try again.');}</script></span>
