68bf30e685405d5ddfa0e0259c16320d75d085b0
Jon Hutter <jon.elliot.hutter@gmail.com>
2017-11-25 12:58:58 -0800

    default placeholder vs value in login form

--- php/login_form.php
+++ php/login_form.php
@@ -1,29 +1,30 @@
 <div id="login">
 			<form action="" method="post">
 			<label style="white-space:pre;"><strong>Username :</strong></label>
-			<input id="name" name="username" placeholder="
+			<input id="name" name="username" 
 				<?php 
 					if (isset($default_username) AND !empty($default_username)){ 
-						print $default_username;}
+						print "value=".$default_username;}
 					else {
-						print "username";
+						print "placeholder='username'";
 					}
 				
 				?>
-				" type="text">
+				
+				 type="text">
 			<br><br>
 			<label style="white-space:pre;"><strong>Password : </strong></label>
-			<input id="password" name="password" placeholder="
+			<input id="password" name="password" 
 			<?php 
 					if (isset($default_pass) AND !empty($default_pass)){ 
-						print $default_pass;}
+						print "value=".$default_pass;}
 					else {
-						print "**********";
+						print "placeholder='**********'";
 					}
 				
 				?>
 			
-			" type="password">
+			 type="password">
 			<br><br>
 			<input class="button" name="submit" type="submit" value="      Login      ">
 			<span><script>var isError = '<?php echo $error; ?>'; if (isError != "") {alert('Invalid password or username. Please try again.');}</script></span>
